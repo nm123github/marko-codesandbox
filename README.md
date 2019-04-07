54594591cd9a39d91de21d131fd19875238e8c1c
Neville Mehta <nevmehta@ebay.com>
2019-04-06 17:03:31 -0700

    implement watch version with webpack (that works)

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-
-# Marko Project Template
-This repo was created using `marko create` command in [`marko-cli`](https://github.com/marko-js/marko-cli). It uses basic [`marko-starter`](https://github.com/marko-js/marko-starter) which is a project for building and running [`Marko.js`](https://github.com/marko-js/marko) applications.
-
-## Starting the server
+Marko Widgets: UI Components Playground
+==================================
 
 ```bash
+git clone https://github.com/marko-js-samples/marko-webpack.git
+cd marko-webpack
+npm install
 npm start
 ```
--- server.js
+++ server.js
@@ -1,3 +1,43 @@
-require("./project").server({
-  httpPort: process.env.PORT || 8080 // Optional, but added here for demo purposes
-});
+
+
+// const express = require('express')
+// // import express from 'express'
+// // import index from './src/components/index.marko'
+// // import markoExpress from 'marko/express';
+// const app = express()
+
+// app.use(express.static(__dirname + "/dist"));
+
+// // app.use(markoExpress());
+
+// // app.get('/', (req, res) => {
+// //   // console.log('hello')
+// //   // res.statusCode = 200;
+// //   // res.setHeader('Content-Type', 'text/plain');
+// //   // res.end('Hello, World!\n');
+
+// //   // index.render({}, res)
+// // });
+
+// app.listen(process.env.PORT || 8080);
+
+// const server = http.createServer((req, res) => {
+//   res.sendfile('dist/index.html');
+// });
+
+// server.listen(process.env.PORT || 8080);
+
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder
+var serve = serveStatic(__dirname + "/dist");
+
+// Create server
+var server = http.createServer(function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+})
+
+// Listen
+server.listen(process.env.PORT || 8080)
